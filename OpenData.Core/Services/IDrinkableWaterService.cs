b6661adb8a22fb872ce996997f5a009948d30d03
DavinaLauwers <davina.lauwers@student.howest.be>
2024-02-15 11:09:21 +0100

    Add IDrinkableWaterService

---
+++ OpenData.Core/Services/IDrinkableWaterService.cs
@@ -0,0 +1,18 @@
+﻿using OpenData.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenData.Core.Services
+{
+    public interface IDrinkableWaterService
+    {
+        Task<IQueryable<DrinkableWater>> GetDrinkableWaterPlaces();
+        void Add(DrinkableWater drinkableWater);
+        void Update(DrinkableWater drinkableWater);
+        void Delete(string id);
+        DrinkableWater GetDetails(string id);
+    }
+}
