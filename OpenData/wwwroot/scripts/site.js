1c4a83fe0fb78ca2d0ca140fdae5ec30aa37833c
DavinaLauwers <davina.lauwers@student.howest.be>
2024-02-20 10:53:19 +0100

    Add delete option

---
+++ OpenData/wwwroot/scripts/site.js
@@ -0,0 +1,12 @@
+﻿window.showConfirmDelete = (id) => {
+    $('#' + id).modal('show');
+};
+
+window.hideConfirmDelete = (id) => {
+    $('#' + id).modal('hide');
+};
+
+
+window.goToDetails = (id) => {
+    $('#' + id).modal('show');
+};
